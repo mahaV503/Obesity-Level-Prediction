c6f25ade573ac17e30df7f8e971e62f665669271
Mahavishnu <mahavishnureddya@gmail.com>
2020-12-03 14:33:10 -0500

    workng server

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import flask
 from flask import request
 
 app=flask.Flask(__name__)
-
-from flask_core import CORS
+from flask_cors import CORS, cross_origin
+#from flask_core import CORS
 
 CORS(app)
 
@@ -11,4 +11,12 @@ CORS(app)
 
 def default():
     return "<h1> API SERVER IS working<h1>"
-    
+
+app.run()
+
+'''
+pip install --global-option=build_ext \
+            --global-option="-I/usr/local/opt/openssl/include" \
+            --global-option="-L/usr/local/opt/openssl/lib" psycopg2
+
+'''
