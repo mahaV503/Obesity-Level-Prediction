d973955c2cec2ca67aeb1df874aaf76077735600
Mahavishnu <mahavishnureddya@gmail.com>
2020-12-03 16:27:15 -0500

    html form

--- app.py
+++ app.py
@@ -10,14 +10,30 @@ model=joblib.load("/Users/mvr/Documents/Mlproject/ObesemodelPoly3.ml")
 app=flask.Flask(__name__)
 #from flask_core import CORS
 
-weight_dic={0:'Insufficient_Weight', 1:'Normal_Weight', 2:'Obesity_Type_I', 3:'Obesity_Type_II',4:'Obesity_Type_III', 5:'Overweight_Level_I', 6:'Overweight_Level_II'}
+weight_dic={0:'Insufficient_Weight', 
+            1:'Normal_Weight', 
+            2:'Obesity_Type_I', 
+            3:'Obesity_Type_II',
+            4:'Obesity_Type_III', 
+            5:'Overweight_Level_I', 
+            6:'Overweight_Level_II'}
+
+
+'''
+top_features=norm_X[["Age","CAEC","family_history_with_overweight",
+"CALC","Weight","CH2O","FAF"]]
+'''
 
 CORS(app)
 
 @app.route("/")
 def default():
     poly=PolynomialFeatures(3)
-    xx=poly.fit_transform([[21,1,1,1,80,3,1]])
+    xx=poly.fit_transform([[request.args['age'],
+                            request.args['CAEC'],
+                            request.args['family_history_with_overweight'],
+                            request.args['CALC'],request.args['Weight'],
+                            request.args['CH2O'],request.args['FAF']]])
     return weight_dic[model.predict(xx)[0]]
 
 app.run()
