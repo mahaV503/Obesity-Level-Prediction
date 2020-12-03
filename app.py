11c41bec436addf68e3662c31e8380acecbf059d
Mahavishnu <mahavishnureddya@gmail.com>
2020-12-03 15:43:28 -0500

    Workn prdictions

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import flask
 from flask import request
-from sklearn.externals import joblib
+#from sklearn.externals import joblib
 from flask_cors import CORS, cross_origin
 from sklearn.preprocessing import PolynomialFeatures
 
+import joblib
 
-model=joblib.load("/content/ObesemodelPoly3.ml")
+model=joblib.load("/Users/mvr/Documents/Mlproject/ObesemodelPoly3.ml")
 app=flask.Flask(__name__)
 #from flask_core import CORS
 
+weight_dic={0:'Insufficient_Weight', 1:'Normal_Weight', 2:'Obesity_Type_I', 3:'Obesity_Type_II',4:'Obesity_Type_III', 5:'Overweight_Level_I', 6:'Overweight_Level_II'}
 
 CORS(app)
 
 @app.route("/")
-
 def default():
     poly=PolynomialFeatures(3)
     xx=poly.fit_transform([[21,1,1,1,80,3,1]])
-    return model.predict(xx)
+    return weight_dic[model.predict(xx)[0]]
 
 app.run()
 
